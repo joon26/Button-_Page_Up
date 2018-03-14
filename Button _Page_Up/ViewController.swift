06ccd12dc8ce86d00c8526307e44fa5cdef9d7b0
D7703_26 <d7703_26@Mac.local>
2018-03-14 12:42:08 +0900

    add button c

--- Button _Page_Up/ViewController.swift
+++ Button _Page_Up/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var a = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
     }
 
+    @IBAction func button(_ sender: Any) {
+        if a == 0 {
+            view.backgroundColor = UIColor.green
+            a = 1
+        }
+        else if a == 1 {
+             view.backgroundColor = UIColor.yellow
+            a = 0
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
